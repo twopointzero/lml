b1034e42da9c2da1c29c1f55e72ca2aab42f801f
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-22 14:59:50 -0700

    Framed out initial Library and Item classes.

---
+++ Lml/Item.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace twopointzero.Lml
+{
+    public class Item
+    {
+        private readonly string _artist;
+        private readonly string _genre;
+        private readonly DateTime _lastPlayed;
+        private readonly string _location;
+        private readonly int _playCount;
+        private readonly double _rating;
+        private readonly string _title;
+
+        public Item(string artist, string title, double rating, int playCount, DateTime lastPlayed, string genre,
+                    string location)
+        {
+            _artist = artist;
+            _title = title;
+            _rating = rating;
+            _playCount = playCount;
+            _lastPlayed = lastPlayed;
+            _genre = genre;
+            _location = location;
+        }
+
+        public string Artist
+        {
+            get { return _artist; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+        }
+
+        public double Rating
+        {
+            get { return _rating; }
+        }
+
+        public int PlayCount
+        {
+            get { return _playCount; }
+        }
+
+        public DateTime LastPlayed
+        {
+            get { return _lastPlayed; }
+        }
+
+        public string Genre
+        {
+            get { return _genre; }
+        }
+
+        public string Location
+        {
+            get { return _location; }
+        }
+    }
+}
---
+++ Lml/Library.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace twopointzero.Lml
+{
+    public class Library
+    {
+        private readonly IEnumerable<Item> _items;
+        private readonly string _sourceType;
+        private readonly string _version;
+
+        public Library(string version, string sourceType, IEnumerable<Item> items)
+        {
+            _version = version;
+            _sourceType = sourceType;
+            _items = items;
+        }
+
+        public string Version
+        {
+            get { return _version; }
+        }
+
+        public string SourceType
+        {
+            get { return _sourceType; }
+        }
+
+        public IEnumerable<Item> Items
+        {
+            get { return _items; }
+        }
+    }
+}
