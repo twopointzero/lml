f5ab0669fb50e35d8cbd21c908d94dc1a1fa9e91
Jeremy Gray <jeremy@jeremygray.ca>
2010-03-23 17:09:31 -0700

    Implemented the beginnings of iTunes Music Library xml file handling (which is in plist xml format.) So far have implemented support for retrieval (as a dictionary) of the top-level library metadata, with support for all plist xml primitive value types and tests of each. This will next be extended to expose all track information.

---
+++ Lml/Importers/AppleITunesXmlImporter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace twopointzero.Lml.Importers
+{
+    public class AppleITunesXmlImporter
+    {
+        public IDictionary<string, object> GetLibraryMetadata(TextReader reader)
+        {
+            if (reader == null)
+            {
+                throw new ArgumentNullException("reader");
+            }
+
+            var doc = XDocument.Load(reader);
+
+            var documentType = doc.DocumentType;
+            var requiredDocumentType = new XDocumentType("plist", "-//Apple Computer//DTD PLIST 1.0//EN",
+                                                         "http://www.apple.com/DTDs/PropertyList-1.0.dtd", "");
+            if (!XNode.DeepEquals(documentType, requiredDocumentType))
+            {
+                throw new ArgumentOutOfRangeException("reader",
+                                                      "The provided TextReader's document does not contain the required DOCTYPE of the form " +
+                                                      @"<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">");
+            }
+
+            var root = doc.Element("plist");
+            if (root == null)
+            {
+                throw new ArgumentOutOfRangeException("reader",
+                                                      "The provided TextReader's document does not contain the required plist root node.");
+            }
+
+            var version = root.Attribute("version");
+            if (version == null || version.Value != "1.0")
+            {
+                throw new ArgumentOutOfRangeException("reader",
+                                                      "The provided TextReader's document does not contain the required plist root node version of \"1.0\".");
+            }
+
+            var mainDict = root.Element("dict");
+            if (mainDict == null)
+            {
+                throw new ArgumentOutOfRangeException("reader",
+                                                      "The provided TextReader's document does not contain the required main dict element.");
+            }
+
+            return mainDict.Elements("key").ToDictionary(key => ((string)key).Replace(" ", ""),
+                                                         key => ImportPrimitive(key.NextNode));
+        }
+
+        private static object ImportPrimitive(XNode node)
+        {
+            var element = node as XElement;
+            if (element == null)
+            {
+                return (string)(XElement)node;
+            }
+
+            var localName = element.Name.LocalName;
+            switch (localName)
+            {
+                case "string":
+                    return element.Value;
+                case "data":
+                    return Convert.FromBase64String(element.Value);
+                case "date":
+                    return XmlConvert.ToDateTime(element.Value, XmlDateTimeSerializationMode.RoundtripKind);
+                case "true":
+                    return true;
+                case "false":
+                    return false;
+                case "real":
+                    return XmlConvert.ToDouble(element.Value);
+                case "integer":
+                    return XmlConvert.ToInt32(element.Value);
+                default:
+                    throw new ArgumentOutOfRangeException("node", node.ToString(),
+                                                          "The provided node is of an unsupported kind.");
+            }
+        }
+    }
+}
---
+++ LmlTests/Importers/AppleITunesXmlImporterTests/GetLibraryMetadata.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using twopointzero.Lml.Importers;
+
+namespace twopointzero.LmlTests.Importers.AppleITunesXmlImporterTests
+{
+    [TestFixture]
+    public class GetLibraryMetadata
+    {
+        [Test]
+        public void GivenMainDictButEmptyShouldReturnEmptyDictionary()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(0, actual.Count);
+        }
+
+        [Test]
+        public void GivenMultipleValuesShouldReturnThem()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><integer>1</integer>
+	<key>Minor Version</key><integer>1</integer>
+	<key>Application Version</key><string>8.0.2</string>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(3, actual.Count);
+            Assert.AreEqual(1, actual["MajorVersion"]);
+            Assert.AreEqual(1, actual["MinorVersion"]);
+            Assert.AreEqual("8.0.2", actual["ApplicationVersion"]);
+        }
+
+        [Test]
+        public void GivenNoDtdShouldThrowArgumentOutOfRangeException()
+        {
+            const string input = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<plist version=""1.0"">
+</plist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+
+        [Test]
+        public void GivenNoMainDictShouldThrowArgumentOutOfRangeException()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+</plist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+
+        [Test]
+        public void GivenNullShouldThrowArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(null));
+        }
+
+        [Test]
+        public void GivenOneDataValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><data>AQID</data>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            CollectionAssert.AreEqual(new byte[] { 1, 2, 3 }, (byte[])actual["MajorVersion"]);
+        }
+
+        [Test]
+        public void GivenOneDateValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><date>2010-03-23T20:58:52Z</date>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(new DateTime(2010, 03, 23, 20, 58, 52, DateTimeKind.Utc), actual["MajorVersion"]);
+        }
+
+        [Test]
+        public void GivenOneFalseValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><false/>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(false, actual["MajorVersion"]);
+        }
+
+        [Test]
+        public void GivenOneIntegerValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><integer>1</integer>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(1, actual["MajorVersion"]);
+        }
+
+        [Test]
+        public void GivenOneRealScientificValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><real>-0.42E+2</real>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(-42, (double)actual["MajorVersion"], 0.001);
+        }
+
+        [Test]
+        public void GivenOneRealValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><real>0.42</real>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(0.42, (double)actual["MajorVersion"], 0.001);
+        }
+
+        [Test]
+        public void GivenOneStringValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><string>1</string>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual("1", actual["MajorVersion"]);
+        }
+
+        [Test]
+        public void GivenOneTrueValueShouldReturnIt()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+<dict>
+	<key>Major Version</key><true/>
+</dict>
+</plist>";
+            var reader = new StringReader(input);
+            var actual = new AppleITunesXmlImporter().GetLibraryMetadata(reader);
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(true, actual["MajorVersion"]);
+        }
+
+        [Test]
+        public void GivenRootNodeWithWrongVersionShouldThrowArgumentOutOfRangeException()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""0.0"">
+</plist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+
+        [Test]
+        public void GivenRootNodeWithoutVersionShouldThrowArgumentOutOfRangeException()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist>
+</plist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+
+        [Test]
+        public void GivenWrongDtdNameShouldThrowArgumentOutOfRangeException()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE zlist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+</plist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+
+        [Test]
+        public void GivenWrongDtdPublicIdShouldThrowArgumentOutOfRangeException()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Snapple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+</plist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+
+        [Test]
+        public void GivenWrongDtdSystemIdShouldThrowArgumentOutOfRangeException()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.snapple.com/DTDs/PropertyList-1.0.dtd"">
+<plist version=""1.0"">
+</plist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+
+        [Test]
+        public void GivenWrongRootNodeShouldThrowArgumentOutOfRangeException()
+        {
+            const string input =
+                @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<!DOCTYPE plist PUBLIC ""-//Apple Computer//DTD PLIST 1.0//EN"" ""http://www.apple.com/DTDs/PropertyList-1.0.dtd"">
+<zlist>
+</zlist>";
+            var reader = new StringReader(input);
+            Assert.Throws<ArgumentOutOfRangeException>(() => new AppleITunesXmlImporter().GetLibraryMetadata(reader));
+        }
+    }
+}
